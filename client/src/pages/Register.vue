2a65a09c8d749d88b0848473263337bb713e5cb7
WDelenclos <wdelenclos@gmail.com>
2019-06-09 14:50:56 +0200

    [Add][Client] Basic Register request

--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -4,18 +4,18 @@
         <div class="row justify-content-md-center">
         <div class="card mt-4 p-4 w-50 align-center">
         <h1 class="mt-2 h2 text-center">Create your account</h1>
-        <form>
+       
             <div class="form-group mt-4">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" v-bind="email" aria-describedby="emailHelp" placeholder="Enter email">
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" v-bind="password" id="password" placeholder="Password">
             </div>
 
-            <button type="submit" class="btn btn-primary w-100 mt-4">Create now !</button>
-        </form>
+            <button class="btn btn-primary w-100 mt-4" v-on:click="register">{{cta}}</button>
+
             <a class="mt-4 text-center" href="/login">I already have an account</a>
         </div>
         </div>
@@ -29,6 +29,31 @@
   export default {
     components: {
       MainLayout
+    },
+    data(){
+        return{
+            cta : 'Create now',
+            email: '',
+            password: ''
+        }
+    },
+   methods: {
+            register() {
+                this.cta = "Register  ...";
+                fetch('http://localhost:3000/register', {
+                    method: 'POST',
+                    headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({email: this.email, password: this.password})
+                }).then((res) => {
+                    console.log(res);
+                })
+                    .catch(function(error) {
+                    console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+                    });
+            }
     }
   }
 </script>
